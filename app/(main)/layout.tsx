b638c28423667c78e16b74b494f453f9f319d851
karthi-prasad77 <karthiprasad44@gmail.com>
2023-12-26 21:43:39 +0530

    feat: Add main page

---
+++ app/(main)/layout.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { Spinner } from "@/components/spinner";
+import { useConvexAuth } from "convex/react";
+import { redirect } from "next/navigation";
+import { Navigation } from "./_components/navigation";
+
+const MainPage = ({
+    children
+} : {
+    children: React.ReactNode
+}) => {
+
+    const {isAuthenticated, isLoading} = useConvexAuth();
+
+    if (isLoading) {
+        return (
+            <div className="h-full flex items-center justify-center">
+                <Spinner size="lg" />
+            </div>
+        )
+    }
+
+    if (!isAuthenticated) {
+        return (
+            redirect("/")
+        )
+    }
+
+    return (
+        <div className="h-full flex dark:bg-[#1F1F1F1]">
+            <Navigation />
+            <main className="flex-1 h-full overflow-y-auto">
+                {children}
+            </main>
+        </div>
+    )
+}
+
+export default MainPage
