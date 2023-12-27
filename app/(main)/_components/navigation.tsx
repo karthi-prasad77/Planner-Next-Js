d6f98024bc46b2f1124a2668d5e0d8092ecb85a4
karthi-prasad77 <karthiprasad44@gmail.com>
2023-12-27 22:01:05 +0530

    feat: Add more features

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,16 +1,22 @@
 "use client";
 
 import { cn } from "@/lib/utils";
-import { ChevronsLeft, MenuIcon } from "lucide-react";
+import { ChevronsLeft, MenuIcon, PlusCircle, Search, Settings } from "lucide-react";
 import { usePathname } from "next/navigation";
 import { ElementRef, useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
 import { UserItem } from "./user-items";
+import { useMutation, useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { Item } from "./item";
+import { toast } from "sonner";
 
 export const Navigation = () => {
 
     const pathname = usePathname();
     const isMobile = useMediaQuery("(max-width: 768px)");
+    const documents = useQuery(api.documents.get);
+    const create = useMutation(api.documents.create);
 
     const isResizingRef = useRef(false);
     const sidebarRef = useRef<ElementRef<"aside">>(null);
@@ -95,6 +101,16 @@ export const Navigation = () => {
         }
       }
 
+      const handleCreate = () => {
+        const promise = create({title: "Untitled"});
+
+        toast.promise(promise, {
+            loading: "Creating a new note....",
+            success: "New note created!",
+            error: "Failed to create a new note"
+        })
+      }
+
     return (
         <>
             <aside
@@ -116,9 +132,16 @@ export const Navigation = () => {
                 </div>
                 <div>
                     <UserItem />
+                    <Item label="Search" icon={Search} isSearch onClick={() => {}} />
+                    <Item label="Settings" icon={Settings} onClick={() => {}} />
+                    <Item onClick={handleCreate} label="New Page" icon={PlusCircle} />
                 </div>
                 <div className="mt-4">
-                    <p>Documents</p>
+                    {documents?.map((document) => (
+                        <p key={document._id}>
+                            {document.title}
+                        </p>
+                    ))}
                 </div>
                 <div 
                     onMouseDown={handleMouseDown}
