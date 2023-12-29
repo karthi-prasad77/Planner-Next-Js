5dcc51d536e8f92ed03612306d9602208c09f8ed
karthi-prasad77 <karthiprasad44@gmail.com>
2023-12-29 20:21:54 +0530

    fix: Make changes on modal provider

---
+++ components/providers/modal-provider.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { SettingsModal } from "../modals/settings-modal";
+import { CoverImageModal } from "../modals/cover-image-modal";
+
+export const ModalProvider = () => {
+
+    const [isMounted, setIsMounted] = useState(false);
+
+    useEffect(() => {
+        setIsMounted(true);
+    }, [])
+
+    if (!isMounted) {
+        return null;
+    }
+
+    return (
+        <>
+            <SettingsModal />
+            <CoverImageModal />
+        </>
+    )
+}
