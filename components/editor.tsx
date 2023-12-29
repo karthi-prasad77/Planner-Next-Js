7b9f78f234260d8ab43cded2621c92675656f228
Karthi Prasad S <107131277+karthi-prasad77@users.noreply.github.com>
2023-12-29 21:39:02 +0530

    fix: type issue

--- components/editor.tsx
+++ components/editor.tsx
@@ -35,11 +35,11 @@ const Editor = ({
     return response.url;
   }
 
-  const editor: BlockNoteEditor = useBlockNote({
+  const editor = useBlockNote({
     editable,
     initialContent: 
       initialContent 
-      ? JSON.parse(initialContent) as PartialBlock[] 
+      ? JSON.parse(initialContent)
       : undefined,
     onEditorContentChange: (editor) => {
       onChange(JSON.stringify(editor.topLevelBlocks, null, 2));
