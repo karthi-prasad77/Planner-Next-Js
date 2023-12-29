1cc2d821a57865d0fa1069b6fdbbc0f8dc611e91
karthi-prasad77 <karthiprasad44@gmail.com>
2023-12-29 20:35:15 +0530

    fix: Add functionalities

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -7,14 +7,17 @@ import { PlusCircle } from "lucide-react";
 import Image from "next/image";
 import { api } from "@/convex/_generated/api";
 import { toast } from "sonner";
+import { useRouter } from "next/navigation";
 
 const DocumentsPage = () => {
 
     const {user} = useUser();
     const create = useMutation(api.documents.create);
+    const router = useRouter();
 
     const onCreate = () => {
-        const promise = create({title: "Untitled"});
+        const promise = create({title: "Untitled"})
+            .then((documentId) => router.push(`/documents/${documentId}`))
 
         toast.promise(promise, {
             loading: "Creating a new note....",
